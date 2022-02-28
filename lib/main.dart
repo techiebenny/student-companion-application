26fd217eb73ef9621bf7ab10a1b767e23c57c283
Ben <techiebenny@outlook.com>
2022-02-28 18:16:17 +0530

    Update main.dart
    Added loginscreen route to main file

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
             return MaterialApp(
               title: 'Student App',
               theme: customTheme(),
-              home: HomeScreen(),
+              home: LoginScreen(),
             );
           }
         });
